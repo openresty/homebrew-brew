e60638b8d32d070ec283ff48f8d0cb6645a1fb57
jiahao <wangjiahao@openresty.com>
2021-09-10 14:54:37 +0800

    openresty-openssl111: upgraded to 1.1.1l.

--- Formula/openresty-openssl111.rb
+++ Formula/openresty-openssl111.rb
@@ -1,13 +1,13 @@
 class OpenrestyOpenssl111 < Formula
   desc "This OpenSSL 1.1.1 library build is specifically for OpenResty uses"
   homepage "https://www.openssl.org/"
-  VERSION = "1.1.1k".freeze
+  VERSION = "1.1.1l".freeze
   revision 1
 
   stable do
     url "https://www.openssl.org/source/openssl-#{VERSION}.tar.gz"
     mirror "https://openresty.org/download/openssl-#{VERSION}.tar.gz"
-    sha256 "892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+    sha256 "0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1"
 
     patch do
       url "https://raw.githubusercontent.com/openresty/openresty/master/patches/openssl-1.1.1f-sess_set_get_cb_yield.patch"
